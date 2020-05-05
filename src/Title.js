eaf3befd822c5c9322a32215fb35aa69596dfc25
gabriellahhh <60484446+gabriellahhh@users.noreply.github.com>
2020-05-05 01:47:58 -0700

    Update Title.js

--- src/Title.js
+++ src/Title.js
@@ -1,16 +1,18 @@
 // src/Title.js
-
+import './Title.css';
 import React from 'react'
 import logo from './logo.svg';
 import './App.css';
 import Title from './Title';
 
-function App() {
+function Title() {
   return (
-    <div className="App">
-      <Title />
+    <div className="Title">
+      <header>
+        <h1>SFPOPOS</h1>
+      </header>
     </div>
-  );
+  )
 }
 
 export default App;
