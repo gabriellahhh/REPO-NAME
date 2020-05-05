f49e65525c7ebf8999346cae68a1797e7ed5c32a
gabriellahhh <60484446+gabriellahhh@users.noreply.github.com>
2020-05-05 01:53:14 -0700

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// src/App.js
+
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Title from './Title';
+import POPOSList from './POPOSList';
+
+function App() {
+  return (
+    <div className="App">
+      <Title />
+      <POPOSList />
+    </div>
+  );
+}
+export default App;
