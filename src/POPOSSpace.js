9bf785dcadbac8523213e0a8f90bff25c8d49f70
gabriellahhh <60484446+gabriellahhh@users.noreply.github.com>
2020-05-12 00:51:43 -0700

    Update POPOSSpace.js

--- src/POPOSSpace.js
+++ src/POPOSSpace.js
@@ -5,7 +5,7 @@ import React from 'react'
 function POPOSSpace() {
   return (
     <div>
-      <img src="" width="300" height="300" alt="Hello" />
+      <img src={`${process.env.PUBLIC_URL}images/50-california-st.jpg`} width="300" height="300" alt="Hello" />
       <h1>Name...</h1>
       <div>Address...</div>
     </div>
@@ -20,14 +20,49 @@ import POPOSSpace from './POPOSSpace';
 ...
 
 function POPOSList() {
+ const { name, image, address } = props
   return (
     <div>
-      <POPOSSpace />
-      <POPOSSpace />
-      <POPOSSpace />
-      <POPOSSpace />
+      <img src={`${process.env.PUBLIC_URL}images/${image}`} width="300" height="300" alt="Hello" />
+      <h1>{name}</h1>
+      <div>{address}</div>
     </div>
   )
 }
-
+function POPOSList() {
+  return (
+    <div className="POPOSList">
+      <POPOSSpace
+        name="50 California Street"
+        address="50 California St."
+        image="50-california-st.jpg"
+      />
+      <POPOSSpace
+        name="100 Pine Street"
+        address="100 Pine St."
+        image="100-pine.jpg"
+      />
+      <POPOSSpace
+        name="101 California Street"
+        address="101 California"
+        image="101-california.jpg"
+      />
+      <POPOSSpace
+        name="343 Sansome Roof Garden"
+        address="343 Sansome St."
+        image="343-sansome-roof-garden.jpg"
+      />
+      <POPOSSpace
+        name="525 Market Street Plaza"
+        address="525 Market St."
+        image="525-market-street-plaza.jpg"
+      />
+      <POPOSSpace
+        name="Citigroup Center"
+        address="1 Sansome St."
+        image="citigroup-center.jpg"
+      />
+    </div>
+  )
+}
 ...
